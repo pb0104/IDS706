b100bee67146c5c8ac0c28bc0a26bf627f6b6ff1
Pranshul Bhatnagar <bpranshul.0104@gmail.com>
2025-08-27 17:44:51 -0400

    Updated code

--- hello.py
+++ hello.py
@@ -1,12 +1,11 @@
 def say_hello(name: str) -> str:
     """Return a greeting message to students in the IDS class."""
-    return f"Hello, {name}, welcome to Data Engineering Systems (IDS 706)!"
-
+    Welcm_msg= f"Hello, {name}, welcome to Data Engineering Systems (IDS 706)!" # welome message
+    return Welcm_msg
 
 def add(a: int, b: int) -> int:
     """Return the sum of two numbers."""
     return a + b
-
 
 if __name__ == "__main__":
     print(say_hello("Prof. Yu"))
