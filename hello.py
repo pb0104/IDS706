1d0d9224ff8dcbf7b997ac2aec232c005a2be7ea
pb0104 <80162070+pb0104@users.noreply.github.com>
2025-08-27 19:31:15 -0400

    Updated hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,15 @@
 def say_hello(name: str) -> str:
     """Return a greeting message to students in the IDS class."""
-    Welcm_msg= f"Hello, {name}, welcome to Data Engineering Systems (IDS 706)!" # welome message
-    return Welcm_msg
+    welcome_msg = (
+        f"Hello, {name}, welcome to Data Engineering Systems (IDS 706)!"
+    )  # welcome message
+    return welcome_msg
+
 
 def add(a: int, b: int) -> int:
     """Return the sum of two numbers."""
     return a + b
+
 
 if __name__ == "__main__":
     print(say_hello("Prof. Yu"))
